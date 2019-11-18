4ba48556116a611a4c9b0be7956f840284a225cd
O Hung Lun <hunglun.o@gmail.com>
2019-11-17 18:32:09 -0800

    completed homework 2

--- 01ProcessAPI/homework02.c
+++ 01ProcessAPI/homework02.c
@@ -4,7 +4,7 @@
 /* happens when they are writing to the file concurrently, i.e., at the */
 /* same time?  */
 
-/* Answer: yes they can. Concurrent access will corrupt the file. */
+/* Tentative Answer: yes they can. Concurrent access will corrupt the file. */
 
 
 
@@ -27,6 +27,22 @@ int main(void){
     execvp(myargs[0], myargs); // runs word count
   }else{
     //    write( fd, "world", 5);
+    char *myargs[3];
+    myargs[0] = strdup("wc"); // program: wc (word count)
+    myargs[1] = strdup("homework01.c"); // arg: file to count
+    myargs[2] = NULL; // mark end of array
+    execvp(myargs[0], myargs); // runs word count    
   }
 
 }
+
+
+/* bennie@bennie-VirtualBox:~/Projects/threeeasypieces/01ProcessAPI$ ./homework02 ; cat hw02.txt  */
+/*   42  228 1267 homework01.c */
+/*   37  174 1178 homework02.c */
+/* bennie@bennie-VirtualBox:~/Projects/threeeasypieces/01ProcessAPI$ ./homework02 ; cat hw02.txt  */
+/*   37  174 1178 homework02.c */
+/*   42  228 1267 homework01.c */
+
+/* The result shows that both processes can access the same file descriptor */
+/* The operating system seems to ensure that each process finishes writing before another process start writing to the same file descriptor. That's surprising to me. */
